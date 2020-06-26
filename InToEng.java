4c9401ca46b1ab33e1e9a5cfe94b0b5e1f9354c8
Nanako Hirobe <65391297+NanakoHirobe@users.noreply.github.com>
2020-06-26 17:19:05 +0900

    Update InToEng.java

--- InToEng.java
+++ InToEng.java
@@ -1,5 +1,13 @@
 import java.util.Scanner;
 public class InToEng {
   public static void main(String[] args) {
-    Scanner sc
+    Scanner sc = new Scanner(System.in);
+    int input = sc.nextInt();//inputに整数が入る
+    System.out.println(translateEng(input));
+    }
+  
+  //数値を英語に変換するメソッド
+  static String translateEng(int n) {
+    return "";
+    }
 }
